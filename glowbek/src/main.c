98ab083f5903cf93c99d4449bf333116d8ee7496
LuminousElement <d.tullemans@gmail.com>
2014-03-21 12:38:16 +1000

    Control multiplexer

--- glowbek/src/main.c
+++ glowbek/src/main.c
@@ -11,54 +11,55 @@ typedef struct cRGB GRB;
 
 int main()
 {
-	/////////////////////////////
-	// Set up the analogue input
-	ADCSRA |= _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0); // Set ADC prescaler to 128 -- 125KHz sample rate @ 16MHz
+    /////////////////////////////
+    // Set up the analogue input
+    ADCSRA |= _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0); // Set ADC prescaler to 128 -- 125KHz sample rate @ 16MHz
 
-	// ADMUX:	REFS1	REFS0	Refernce
-	//			0		0		External ARef
-	//			0		1		Vcc
-	//			1		1		Internal 1.1V (ATMega168/328) / 2.56V (ATMega8)
+    // ADMUX:   REFS1   REFS0   Refernce
+    //          0       0       External ARef
+    //          0       1       Vcc
+    //          1       1       Internal 1.1V (ATMega168/328) / 2.56V (ATMega8)
 
-	ADMUX |= _BV(REFS0) | _BV(REFS1);
-	ADMUX |= _BV(ADLAR); // Make the ADC left-align the values. We then read bits 9 to 2 from ADCH (ignoring bits 0 and 1 in ADCL).
+    ADMUX  = _BV(MUX0); // Set ADC multiplexer to read from channel 1 (PORTC 1)
+    ADMUX |= _BV(REFS0) | _BV(REFS1);
+    ADMUX |= _BV(ADLAR); // Make the ADC left-align the values. We then read bits 9 to 2 from ADCH (ignoring bits 0 and 1 in ADCL).
 
-	ADCSRA |= _BV(ADATE); // Enable the Analog-Digital Auto Trigger Event
+    ADCSRA |= _BV(ADATE); // Enable the Analog-Digital Auto Trigger Event
 
-	ADCSRA |= _BV(ADEN); // Enable the ADC
-	ADCSRA |= _BV(ADSC); // Start ADC measurements
+    ADCSRA |= _BV(ADEN); // Enable the ADC
+    ADCSRA |= _BV(ADSC); // Start ADC measurements
 
-	GRB frameBuffer[40];
-	memset(frameBuffer, 0, 3 * 40);
+    GRB frameBuffer[40];
+    memset(frameBuffer, 0, 3 * 40);
 
-	uint8_t valBuffer[40];
-	memset(valBuffer, 0, 40);
+    uint8_t valBuffer[40];
+    memset(valBuffer, 0, 40);
 
-	uint16_t hue = 0;
+    uint16_t hue = 0;
 
-	while(1) {
+    while(1) {
 
-		hue += 5;
-		if (hue > MAX_HUE) hue = 0;
+        hue += 5;
+        if (hue > MAX_HUE) hue = 0;
 
-		uint16_t out = ADCH * 5 + valBuffer[39];
+        uint16_t out = ADCH * 5 + valBuffer[39];
 
-		for (int i = 39; i > 0; --i)
-		{
-			int v = valBuffer[i-1] - 2;
-			valBuffer[i] = v < 0 ? 0 : v;
-		}
+        for (int i = 39; i > 0; --i)
+        {
+            int v = valBuffer[i-1] - 2;
+            valBuffer[i] = v < 0 ? 0 : v;
+        }
 
-		valBuffer[0] = (out > 255) ? 255 : out;
+        valBuffer[0] = (out > 255) ? 255 : out;
 
 
-		for (int i=0; i<40; i++) {
+        for (int i=0; i<40; i++) {
 
-			cHSV colour = { hue + i * 19, MAX_SAT, valBuffer[i] };
-			frameBuffer[i] = ToRgb(&colour);
-		}
+            cHSV colour = { hue + i * 19, MAX_SAT, valBuffer[i] };
+            frameBuffer[i] = ToRgb(&colour);
+        }
 
-		ws2812_setleds(frameBuffer, 40);
-		_delay_ms(15);
-	}
+        ws2812_setleds(frameBuffer, 40);
+        _delay_ms(15);
+    }
 }
